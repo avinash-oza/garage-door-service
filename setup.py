f92d1315b2316ed33c8f2d481eb0b56661224645
Avi Oza <avioza@gmail.com>
2018-12-29 17:57:16 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='garage-door',
-        version='0.1',
+        version='0.2b1',
         packages=['garage_door',],
         scripts=['bin/run-garage-door-service'],
         license='TBD',
