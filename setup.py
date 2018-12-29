ae29006442287b042292303f619ffb176218f847
Avi Oza <avioza@gmail.com>
2018-12-29 11:12:34 -0500

    initial commit to refactor to a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name='garage-door',
+        version='0.1',
+        packages=['garage_door',],
+        # scripts=['bin/run-telegram-bot'],
+        license='TBD',
+        long_description='TBD'
+        )
