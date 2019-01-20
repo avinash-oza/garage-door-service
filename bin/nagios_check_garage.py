cfc70734414196a389654f88810593a61dfb2658
Avi Oza <avioza@gmail.com>
2019-01-20 17:40:32 -0500

    Update script to submit checks

--- bin/nagios_check_garage.py
+++ bin/nagios_check_garage.py
@@ -2,6 +2,7 @@
 import sys
 import requests
 import argparse
+import warnings
 
 # config is specified by env variable APP_SETTINGS
 def check_garage(api_endpoint):
@@ -25,16 +26,19 @@ def check_garage(api_endpoint):
 def submit_check(api_endpoint, passive_check_endpoint, token, hostname):
     status_text, exit_code = check_garage(api_endpoint)
     submit_cmd = "PROCESS_SERVICE_CHECK_RESULT;{};Garage Status;{};{}".format(hostname, exit_code, status_text)
-    resp = requests.get(passive_check_endpoint, params={'token': token,
-                                                        'cmd': 'submitcmd',
-                                                        'command': submit_cmd})
+    with warnings.catch_warnings():
+        # ignore the SSL related warning as this is internal
+        warnings.simplefilter("ignore")
+        resp = requests.get(passive_check_endpoint, verify=False, params={'token': token,
+                                                            'cmd': 'submitcmd',
+                                                            'command': submit_cmd})
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--api-endpoint', type=str, help="Garage status API endpoint")
     parser.add_argument('--token', type=str, help="Nagios passive check token")
     parser.add_argument('--submit-check-url', type=str, help="Nagios passive check url")
-    parser.add_argument('--shostname', type=str, help="Nagios passive check url")
+    parser.add_argument('--hostname', type=str, help="Hostname of check on nagios")
     args = parser.parse_args()
 
     submit_check(args.api_endpoint, args.submit_check_url, args.token, args.hostname)
