902ea72380136739cd15381a8f9165d29e8533ed
Avi Oza <avioza@gmail.com>
2019-12-26 22:54:24 -0500

    drop config reading

--- garage_door/__init__.py
+++ garage_door/__init__.py
@@ -3,9 +3,9 @@ from flask_restplus import Api
 
 app = Flask(__name__)
 
-app.config.from_mapping({'GENERAL_HOSTNAME': 'DEFAULT_HOST'})
-if not app.config.from_envvar('APP_SETTINGS', silent=True):
-    print("Did not find a config to load")
+# app.config.from_mapping({'GENERAL_HOSTNAME': 'DEFAULT_HOST'})
+# if not app.config.from_envvar('APP_SETTINGS', silent=True):
+#     print("Did not find a config to load")
 
 from .status import api as ns1
 
