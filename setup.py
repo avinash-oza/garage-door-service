f694e3518a11c0199bb625afd6df3c0d1e2f7ec0
Avi <avinash-oza@users.noreply.github.com>
2020-12-12 09:32:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='garage-door',
-        version='0.2b14',
+        version='0.2b15',
         packages=['garage_door',],
         license='TBD',
         long_description='TBD'
